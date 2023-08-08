ed563f4bba3e05c9ecbfc05d079c56596d3a762b
lilale <alessandrotambellini03@gmail.com>
2023-08-08 13:10:05 +0200

    create common.h file to include common libraries, constants and functions

---
+++ common.h
@@ -0,0 +1,19 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define PORTNUM 3000
+
+// here the common functions
+
+#endif
--- server.c
+++ server.c
@@ -1,14 +1,5 @@
-#include <arpa/inet.h>
-#include <errno.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include "common.h"
 
-#define PORTNUM 2300
 #define MAX_HOSTNAME_SIZE 100
 
 int mistring()
