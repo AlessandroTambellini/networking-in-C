ff81440f132ab0d4061617dbd5ddbcae03547366
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2024-02-11 12:48:27 +0100

    - wrap start msg in a function - add command to clear terminal

--- client-server/client.c
+++ client-server/client.c
@@ -6,6 +6,8 @@
 typedef struct sockaddr_in sockaddr_in;
 typedef struct sockaddr sockaddr;
 
+void
+showStartMsg(void);
 void 
 startCodingSession(int sock_FD, char req[], char res[]);
 
@@ -26,12 +28,11 @@ main(void)
     if (isConnected == -1) 
         handle_exit("Unable to connect socket");
 
-    printf(CYAN "Connected to server on port %d successfully\n" RESET, PORT);
-    printf("Type HELP in the req to know the commands\n\n");
-
+    showStartMsg();
+    
     char res[RES_LEN], req[REQ_LEN];
     ssize_t req_len = REQ_LEN, res_len = 0;
-    bool isReqTooLong = false;
+    bool isReqTooLong = false, isReqEmpty = false;
 
     while (1)
     {
@@ -44,17 +45,27 @@ main(void)
 
             // fgets adds \0 at the end of the string, even if is overflowing. Plus, when you enter, you add '\n'. Therefore it is string + '\n' + '\0'. So, max input is 253 chars
             fgets(req, REQ_LEN, stdin);
-            // remove all the chars still in the stream if the req was > 255
+            // discard all pending chars in the stream if the req was > 255
             __fpurge(stdin);
 
             isReqTooLong = req[REQ_LEN - 1] == 'j' ? false : true;
             if (isReqTooLong)
                 printf(RED BOLD "The req is too long!\n" RESET);
+            isReqEmpty = strcmp(req, "\n") == 0 ? true : false;
 
             // '\n' is read by the server and if so is not able to understand the req
             req[strcspn(req, "\n")] = '\0';
 
-        } while (isReqTooLong);
+        } while (isReqTooLong || isReqEmpty);
+
+        // exceptional req not sent to server
+        if (strcmp(req, CLEAR) == 0)
+        {
+            system("clear");
+            showStartMsg();
+            // I do not want to nest if-else for a single case
+            continue;
+        }
 
         // 1) send the req
         req_len = send(client_socket_FD, req, strlen(req) + 1, 0); 
@@ -147,4 +158,10 @@ void startCodingSession(int sock_FD, char req[], char res[])
         printf(MAGENTA "\t" "< " "ok");
 
     printf(RESET "\n[TERMINAL_CLOSE]\n\n");
+}
+
+void showStartMsg(void)
+{
+    printf(CYAN "Connected to server on port %d successfully\n" RESET, PORT);
+    printf("Type HELP in the req to know the commands\n\n");
 }
